49eefc3ee1587778bc021b083e42343107a739a7
Kelvin Anderson <kelvinanders17@gmail.com>
2022-08-16 16:37:29 +0700

    Gabisa design :(

--- src/pages/Login/LoginView.js
+++ src/pages/Login/LoginView.js
@@ -11,6 +11,13 @@ export const LoginView = () => {
                 <p>ASSET MANAGEMENT</p>
                 <h4>Log In to Dashboard</h4>
                 <h6>Enter your email and password below</h6>
+                <form className='formContainer'>
+                    <label>EMAIL</label>
+                    <input type='email' placeholder='Email address'></input>
+                    <label>PASSWORD</label>
+                    <input type='password' placeholder='Password'></input>
+                    <button type='submit'>Log In</button>
+                </form>
             </div>
         </div>
     )
