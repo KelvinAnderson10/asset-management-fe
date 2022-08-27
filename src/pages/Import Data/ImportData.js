4c45c27c240eb9069ebf59bacd5747719f1ce4f5
Utsman Arifin <utsmanarifin@mail.ugm.ac.id>
2022-08-27 17:22:42 +0700

    [ADD] loading screen

--- src/pages/Import Data/ImportData.js
+++ src/pages/Import Data/ImportData.js
@@ -3,6 +3,10 @@ import Sidebar from '../../shared/components/Sidebar/Sidebar'
 import * as XLSX from 'xlsx';
 import { useDeps } from '../../shared/context/DependencyContext';
 import './ImportData.css'
+import guidelines from '../../guidelines.xlsx'
+import template from '../../template.xlsx'
+import Loading from '../../shared/components/Loading/Loading';
+
 export const ImportData = () => {
   const [excelData, setExcelData] = useState([])
   const [doneUploadExcel, setDoneUploadExcel] = useState(false)
@@ -30,21 +34,23 @@ export const ImportData = () => {
   }
 
   const readUploadFile = (e) => {
-    setIsLoading(true)
     e.preventDefault();
-    if (e.target.files) {
+    if (e.target.files[0]) {
+        setIsLoading(true)
         const reader = new FileReader();
         reader.onload = (e) => {
             const data = e.target.result;
             const workbook = XLSX.read(data, { type: "array" });
             const sheetName = workbook.SheetNames[0];           // Sheetnya, misal produk berarti di index ke 2
             const worksheet = workbook.Sheets[sheetName];
-            const json = XLSX.utils.sheet_to_json(worksheet);
-
+            let json = XLSX.utils.sheet_to_json(worksheet);
+            
             // Notes : yang dikirim ke backend tetap const json, const excelData hanya untuk tampilan
             setExcelData(json)
 
             setUploadBackendData(json)
+            e.target.files = null
+            setIsLoading(false)
         };
         reader.readAsArrayBuffer(e.target.files[0]);
     }
@@ -59,131 +65,140 @@ export const ImportData = () => {
   return (
     <div>
         <Sidebar/>
-        <div className='import-container'>
-          <div className='import-card'>
-            <div className='title-container'>
-              <h4>ADD MULTIPLE ASSET</h4>
-            </div>
-        <div class='form-upload-container'>
-          <form>
-            <label htmlFor="upload">Upload File</label>
-            <input
-                type="file"
-                name="upload"
-                id="upload"
-                onChange={readUploadFile}
-            />
-          </form>
-        </div>
-        
         <div>
-          <div className="import-box">
-            <br></br>
-                  <h2>
-                      Upload <b>Assets</b>
-                  </h2>
-            <div className="table-responsive">
-              <div className="table-wrapper">
-                <div className="table-title">
-                  <div className="row">
-                    <div className="col-xs-8">
+            <div className='import-container'>
+              <div className='import-card'>
+                <div className='title-container'>
+                  <h4>ADD MULTIPLE ASSET</h4>
+                </div>
+            <div class='form-upload-container'>
+              <form>
+                <label htmlFor="upload">Upload File</label>
+                <input
+                    type="file"
+                    name="upload"
+                    id="upload"
+                    onChange={readUploadFile}
+                />
+              </form>
+              <button>
+                  <a href={guidelines} download="Upload Guidelines.xlsx">Download Guidelines</a>
+                  </button>
+                  <button>
+                  <a href={template} download="Template.xlsx">Download Template</a>
+              </button>
+            </div>
+            
+            <div>
+              <div className="import-box">
+                <br></br>
+                      <h2>
+                          Upload <b>Assets</b>
+                      </h2>
+                <div className="table-responsive">
+                  <div className="table-wrapper">
+                    <div className="table-title">
+                      <div className="row">
+                        <div className="col-xs-8">
+                        </div>
+                      </div>
                     </div>
+
+                    <table className="table table-striped table-hover">
+                      <thead>
+                        <tr>
+                          <th>Tanggal Pembelian</th>
+                          <th>Tahun</th>
+                          <th>No. PO / Dokumenen Pendukung</th>
+                          <th>Vendor</th>
+                          <th style={{minWidth: "300px"}}>Nama Barang</th>
+                          <th>Harga Perolehan</th>
+                          <th>PPN</th>
+                          <th>Biaya Lain-Lain</th>
+                          <th>Total Harga Perolehan</th>
+                          <th>Jenis Produk</th>
+                          <th>Kategori Jenis Produk</th>
+                          <th>Kategori Aset Tetap</th>
+                          <th>BAST</th>
+                          <th>Kondisi</th>
+                          <th>Insurance</th>
+                          <th>Lokasi</th>
+                          <th>User</th>
+                          <th>Jabatan</th>
+                          <th>Initisal</th>
+                          <th>Kode Wilayah</th>
+                          <th>Kode Asset</th>
+                          <th>Tahun Pembelian</th>
+                          <th>Kode Urut barang</th>
+                          <th>Nomor Asset</th>
+                          <th>Masa Manfaat (Bulan)</th>
+                          <th>Penyusutan Perbulan</th>
+                          <th>Total Bulan Penyusutan</th>
+                          <th>Total Penyusutan</th>
+                          <th>Nilai Asset saat ini</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {doneUploadExcel && 
+                        excelData.map((item, index) => {
+                          return (
+                            <tr key={item['Nomor Asset']}>
+                              <td>{item['Tanggal Pembelian'] && convertExcelDate(item['Tanggal Pembelian'])}</td>
+                              <td>{item['Tahun']}</td>
+                              <td>{item['No. PO / Dokumenen Pendukung']}</td>
+                              <td>{item['Vendor']}</td>
+                              <td style={{minWidth: "300px"}}>{item['Nama Barang']}</td>
+                              <td>{item['Harga Perolehan']}</td>
+                              <td>{item['PPN']}</td>
+                              <td>{item['Biaya Lain-Lain']}</td>
+                              <td>{item['Total Harga Perolehan']}</td>
+                              <td>{item['Jenis Produk']}</td>
+                              <td>{item['Kategori Jenis Produk']}</td>
+                              <td>{item['Kategori Aset Tetap']}</td>
+                              <td>{item['BAST'] && convertExcelDate(item['BAST'])}</td>
+                              <td>{item['Kondisi']}</td>
+                              <td>{item['Insurance']}</td>
+                              <td>{item['Lokasi']}</td>
+                              <td>{item['User']}</td>
+                              <td>{item['Jabatan']}</td>
+                              <td>{item['Initisal']}</td>
+                              <td>{item['Kode Wilayah']}</td>
+                              <td>{item['Kode Asset']}</td>
+                              <td>{item['Tahun Pembelian']}</td>
+                              <td>{item['Kode Urut barang']}</td>
+                              <td>{item['Nomor Asset']}</td>
+                              <td>{item['Masa Manfaat (Bulan)']}</td>
+                              <td>{item['Penyusutan Perbulan']}</td>
+                              <td>{item['Total Bulan Penyusutan']}</td>
+                              <td>{item['Total Penyusutan']}</td>
+                              <td>{item['Nilai Asset saat ini']}</td>
+                            </tr>
+                          )
+                        })}
+                      </tbody>
+                    </table>
                   </div>
                 </div>
-
-                <table className="table table-striped table-hover">
-                  <thead>
-                    <tr>
-                      <th>Tanggal Pembelian</th>
-                      <th>Tahun</th>
-                      <th>No. PO / Dokumenen Pendukung</th>
-                      <th>Vendor</th>
-                      <th style={{minWidth: "300px"}}>Nama Barang</th>
-                      <th>Harga Perolehan</th>
-                      <th>PPN</th>
-                      <th>Biaya Lain-Lain</th>
-                      <th>Total Harga Perolehan</th>
-                      <th>Jenis Produk</th>
-                      <th>Kategori Jenis Produk</th>
-                      <th>Kategori Aset Tetap</th>
-                      <th>BAST</th>
-                      <th>Kondisi</th>
-                      <th>Insurance</th>
-                      <th>Lokasi</th>
-                      <th>User</th>
-                      <th>Jabatan</th>
-                      <th>Initisal</th>
-                      <th>Kode Wilayah</th>
-                      <th>Kode Asset</th>
-                      <th>Tahun Pembelian</th>
-                      <th>Kode Urut barang</th>
-                      <th>Nomor Asset</th>
-                      <th>Masa Manfaat (Bulan)</th>
-                      <th>Penyusutan Perbulan</th>
-                      <th>Total Bulan Penyusutan</th>
-                      <th>Total Penyusutan</th>
-                      <th>Nilai Asset saat ini</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {doneUploadExcel && 
-                    excelData.map((item, index) => {
-                      return (
-                        <tr key={item['Nomor Asset']}>
-                          <td>{item['Tanggal Pembelian'] && convertExcelDate(item['Tanggal Pembelian'])}</td>
-                          <td>{item['Tahun']}</td>
-                          <td>{item['No. PO / Dokumenen Pendukung']}</td>
-                          <td>{item['Vendor']}</td>
-                          <td style={{minWidth: "300px"}}>{item['Nama Barang']}</td>
-                          <td>{item['Harga Perolehan']}</td>
-                          <td>{item['PPN']}</td>
-                          <td>{item['Biaya Lain-Lain']}</td>
-                          <td>{item['Total Harga Perolehan']}</td>
-                          <td>{item['Jenis Produk']}</td>
-                          <td>{item['Kategori Jenis Produk']}</td>
-                          <td>{item['Kategori Aset Tetap']}</td>
-                          <td>{item['BAST'] && convertExcelDate(item['BAST'])}</td>
-                          <td>{item['Kondisi']}</td>
-                          <td>{item['Insurance']}</td>
-                          <td>{item['Lokasi']}</td>
-                          <td>{item['User']}</td>
-                          <td>{item['Jabatan']}</td>
-                          <td>{item['Initisal']}</td>
-                          <td>{item['Kode Wilayah']}</td>
-                          <td>{item['Kode Asset']}</td>
-                          <td>{item['Tahun Pembelian']}</td>
-                          <td>{item['Kode Urut barang']}</td>
-                          <td>{item['Nomor Asset']}</td>
-                          <td>{item['Masa Manfaat (Bulan)']}</td>
-                          <td>{item['Penyusutan Perbulan']}</td>
-                          <td>{item['Total Bulan Penyusutan']}</td>
-                          <td>{item['Total Penyusutan']}</td>
-                          <td>{item['Nilai Asset saat ini']}</td>
-                        </tr>
-                      )
-                    })}
-                  </tbody>
-                </table>
+                  <br></br>
+                  <br></br>
+                  <br></br>
+                  <br></br>
+                  <button onClick={handleSubmit}>Save</button>
+                  <button>Clear</button>
+                  <br></br>
+                  <br></br>
+                  <br></br>
+                  <br></br>
+                  <br></br>
               </div>
             </div>
-              <br></br>
-              <br></br>
-              <br></br>
-              <br></br>
-              <button onClick={handleSubmit}>Save</button>
-              <button>Clear</button>
-              <br></br>
-              <br></br>
-              <br></br>
-              <br></br>
-              <br></br>
-          </div>
-        </div>
 
-        {/* tinggal didesign */}
-        {isLoading && <div>Lagi loading</div>}
-        </div>   
+            {/* tinggal didesign */}
+            </div>   
+            </div>
+            {isLoading && <Loading/>}
         </div>
+        
     </div>
   )
 }
